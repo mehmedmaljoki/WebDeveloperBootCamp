01d3c5b51ca305a6377a3ab98f2a9cec6787480a
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2021-11-09 16:35:27 +0100

    javaScript push, pop, shift and unshift

--- javaScriptBasics/arrays.js
+++ javaScriptBasics/arrays.js
@@ -6,3 +6,20 @@ let numbers = [1, 2, 3, 4, 5, 6, 67];
 
 // a mixed Array
 let stuff = [true, 68, 'cat', null];
+
+// add to end of the array
+stuff.push("meski"); // gives the new length of the array
+
+// remove from the end of the array
+numbers.pop(); // liefert das entfernte element
+
+// remove from start of the array
+let meski = ['meski', 'meskitest']
+meski.shift(); // liefert das entferne element 
+
+// add to start of the array
+let test = ['meski', 'meskitest']
+test.unshift(); // liefert die neue laenge
+
+
+// other Arraz methods
