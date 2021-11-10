43912a881191846768b645b005d0fdfaeba5c363
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2021-11-10 10:53:09 +0100

    objects starting...

---
+++ javaScriptBasics/objects.js
@@ -0,0 +1,26 @@
+const fitBitData = {
+    totalSteps: 308727,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    workoutsThisWeek: '5 of 7',
+    avgGoodSleep: '2:13'
+};
+
+const person = {
+    firstName: 'Meski',
+    lastName: 'Jagger',
+
+};
+
+const comment = {
+    username: 'meski',
+    downVotes: 19,
+    upVotes: 214,
+    netScore: 195,
+    commentText: 'nice pic, lol',
+    tags: ['#hilarious', '#funny', '#silly'],
+    isGillded: false
+};
+
+typeof [];
+
