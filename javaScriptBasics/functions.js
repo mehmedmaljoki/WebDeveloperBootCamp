898ea6981dd1e198ea8541bec29c9884b5176108
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2021-11-11 21:46:52 +0100

    more with functions

--- javaScriptBasics/functions.js
+++ javaScriptBasics/functions.js
@@ -1,6 +1,17 @@
 // defining functions
+function run() {
+
+}
+
+run();
 
 // functions Argumment
+
+function greet(firstName, lastName) {
+    console.log(`Hey there ${firstName} ${lastName}`);
+}
+
+greet(Muhammad, Ali);
 
 // function return values
 
