b0547cc48479781b8fbf38e7633882936013839d
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2021-11-16 11:45:26 +0100

    some and every

--- javaScriptBasics/arraysFunctions.js
+++ javaScriptBasics/arraysFunctions.js
@@ -47,9 +47,30 @@ const smallNums = myNums.filter(n => n < 5);
 
 // reduce
 
-// some
+// some -> if one element from Array passes is TRUE
+const words = ['dog', 'jello', 'log', 'cupcake', 'bag', 'wag'];
+words.some(word => {
+    return word.length > 4;
+}) //true
 
-// every
+// do any words start with 'Z'
+words.some(word => word[0] === "Z"); // false
+
+// do any words contain "cake"?
+words.some(w => w.includes('cake')); // true
+
+
+
+
+// every -> if every element passes the test is TRUE
+words.every(word => {
+    return word.length === 3;
+}) //true
+
+words.every(w => {
+    let lastLetter = w[w.length - 1];
+    return lastLetter === 'g'
+}) // true
 
 
 setTimeout(() => {
